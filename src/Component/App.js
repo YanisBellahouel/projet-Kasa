0186167fb06a2348dbe8d44039d0a93a39b380b7
yanisbellahouel <yanis.bellahouel76@gmail.com>
2023-04-03 16:00:03 +0200

    up

--- src/Component/App.js
+++ src/Component/App.js
@@ -3,7 +3,7 @@ import ShoppingList from './ShoppingList'
 import ElementList from './elementList'
 
 function App() {
-    return <div><Banner /> <ElementList /> </div>
+    return <div><Banner /> <ShoppingList /> <ElementList /> </div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './Component/App';
-
+// import { BrowserRouter as Router } from 'react-router-dom'
+ 
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Router>
+//             <App />
+//         </Router>
+//     </React.StrictMode>,
+// document.getElementById('root')
+// )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
