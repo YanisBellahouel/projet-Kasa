fc283a12f89e0e007a590ea9638610f84467091b
yanisbellahouel <yanis.bellahouel76@gmail.com>
2023-04-07 20:30:45 +0200

    up

--- src/Component/card/Card.js
+++ src/Component/card/Card.js
@@ -6,12 +6,15 @@ function Card () {
     
     return (
         <div className='kasa-card'>
-        {Element.map((logement, id) => (
-            <img key={logement.id} src={logement.cover}
-             alt='logement' className='card-img'/>    
+            {Element.map((logement, id) => (
+                <div>
+                <img  key={logement.id} src={logement.cover}
+                alt='logement' className='card-img'/> 
+                <p key={logement.title} className='card-title'> {logement.title}  </p>
+                </div> 
         
-    ))}
-    </div>
+            ))}
+        </div>
     )
 
 }
